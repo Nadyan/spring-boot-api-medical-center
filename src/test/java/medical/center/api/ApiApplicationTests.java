25866baa71cc324d4c7b910d41f6da733d9af01c
Nadyan <nadyan_sp@hotmail.com>
2022-11-17 10:39:38 -0300

    hello world controller

---
+++ src/test/java/medical/center/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package medical.center.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
