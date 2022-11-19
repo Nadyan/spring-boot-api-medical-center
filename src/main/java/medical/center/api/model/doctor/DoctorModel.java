345902a55cae70555486260d1ddea3327b251d27
Nadyan <nadyan_sp@hotmail.com>
2022-11-19 11:32:58 -0300

    add new migration

--- src/main/java/medical/center/api/model/doctor/DoctorModel.java
+++ src/main/java/medical/center/api/model/doctor/DoctorModel.java
@@ -10,6 +10,7 @@ import medical.center.api.model.msc.AddressModel;
 public record DoctorModel(
         @NotBlank String name,
         @NotBlank @Email String email,
+        @NotBlank String phone,
         @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
         @NotNull Specialty specialty,
         @NotNull @Valid AddressModel address) {
